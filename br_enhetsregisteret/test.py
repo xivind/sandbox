665a94cc14f77c48be77170504d8fe3b47435f7d
Øivind Langeland <oivind.langeland@gmail.com>
2022-03-22 07:43:43 +0100

    Endringer i test-script

--- br_enhetsregisteret/test.py
+++ br_enhetsregisteret/test.py
@@ -1,7 +1,7 @@
 """Code to retrieve and parse data from Enhetsregisteret"""
 #!/usr/bin/python3
 
-from lib2to3.pytree import convert
+#from lib2to3.pytree import convert Vurder å fjerne denne
 import os
 from datetime import datetime
 from os.path import exists
@@ -32,10 +32,68 @@ def prepare_full_dataset():
     
 def prepare_dataframe():
     print("Gjør klar dataframe...")
+
+    df = pd.read_csv('er.csv', dtype={
+        'Organisasjonsnummer': str,
+        'Navn': str,
+        'Organisasjonsform.kode': 'category',
+        'Organisasjonsform.beskrivelse': 'category',
+        'Næringskode 1': str,
+        'Næringskode 1.beskrivelse': str,
+        'Næringskode 2': str,
+        'Næringskode 2.beskrivelse': str,
+        'Næringskode 3': str,
+        'Næringskode 3.beskrivelse': str,
+        'Hjelpeenhetskode': 'category',
+        'Hjelpeenhetskode.beskrivelse': 'category',
+        'Antall ansatte': np.int16,
+        'Hjemmeside': str,
+        'Postadresse.adresse': str,
+        'Postadresse.poststed': str,
+        'Postadresse.postnummer': str,
+        'Postadresse.kommune': str,
+        'Postadresse.kommunenummer': str,
+        'Postadresse.land': 'category',
+        'Postadresse.landkode': 'category',
+        'Forretningsadresse.adresse': str,
+        'Forretningsadresse.poststed': str,
+        'Forretningsadresse.postnummer': str,
+        'Forretningsadresse.kommune': str,
+        'Forretningsadresse.kommunenummer': str,
+        'Forretningsadresse.land': 'category',
+        'Forretningsadresse.landkode': 'category',
+        'Institusjonell sektorkode': 'category',
+        'Institusjonell sektorkode.beskrivelse': 'category',
+        'Siste innsendte årsregnskap': str, # klarte ikke konvertere til np.int16
+        'Registreringsdato i Enhetsregisteret': str, # klarer ikke konvertere 'datetime64',
+        'Stiftelsesdato': str, # klarte ikke å konvertere til datetime64 - 1550-12-31 00:00:00
+        'FrivilligRegistrertIMvaregisteret': 'category',
+        'Registrert i MVA-registeret': 'category',
+        'Registrert i Frivillighetsregisteret': 'category',
+        'Registrert i Foretaksregisteret': 'category',
+        'Registrert i Stiftelsesregisteret': 'category',
+        'Konkurs': 'category',
+        'Under avvikling': 'category',
+        'Under tvangsavvikling eller tvangsoppløsning': 'category',
+        'Overordnet enhet i offentlig sektor': str,
+        'Målform': 'category' })
+
+    # Henter ut relevante kolonner
+    df2 = df[["Organisasjonsnummer", "Navn", 'Organisasjonsform.kode', "Organisasjonsform.beskrivelse", "Næringskode 1", "Næringskode 1.beskrivelse", "Postadresse.adresse", "Postadresse.kommune", "Registreringsdato i Enhetsregisteret", "Registrert i MVA-registeret"]]
+
+    # Konverterer datatype i alle kolonner til string
+    df3 = df2.astype(str)
+
+    # Liste med næringskoder for å sortere
+    searchfor = ["86.1, 86.10, 86.101, 86.102, 86.103, 86.104, 86.105, 86.106, 86.107, 86.2, 86.21, 86.211, 86.212, 86.22, 86.221, 86.222, 86.223, 86.224, 86.225, 86.23, 86.230, 86.9, 86.90, 86.901, 86.902, 86.903, 86.904, 86.905, 86.906, 86.907, 87, 87.1, 87.10, 87.101, 87.102, 87.2, 87.20, 87.201, 87.202, 87.203, 87.3, 87.30, 87.301, 87.302, 87.303, 87.304, 87.305, 87.9, 87.90, 87.901, 87.909"]
+
+    # Dataframe med filtrering på næringskoder i liste
+    enheter = df3[df3['Næringskode 1'].str.contains('|'.join(searchfor))]
     
 
 def write_dataframe_to_csv():
     print("Skriver dataframe til csv...")
+    enheter.to_csv(OUTFILE) 
     
 
 if exists(FULLDATASET) == True:
@@ -52,7 +110,7 @@ if exists(FULLDATASET) == True:
         quit()
 
 if exists(FULLDATASET) == False:
-    print("Fant ikke fullt datasett...")
+    print("Fant ikke fullt datasett")
     prepare_full_dataset()
 
 prepare_dataframe()
