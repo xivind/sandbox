e2c09d33e989749bf69d8f0f3768b067dae6218d
Xivind <oivind.langeland@gmail.com>
2022-09-24 09:14:34 +0200

    Added scripts for manual and automatic retrieving strava data and sending on mqtt

---
+++ strava/create-container-send-strava.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Build the image and tag it
+docker build -t strava -f send-strava.Dockerfile .
+
+# Create the container
+docker run -d \
+  --name=strava \
+  -e TZ=Europe/Stockholm \
+  -v /home/pi/code/secrets:/secrets \
+  --restart unless-stopped \
+  strava \
+  ./send_strava.py \
+  --oauth_file /secrets/strava_tokens.json \
+  --mqtt_host messagebroker \
+  --mqtt_port 1883 \
+  --mqtt_topic strava \
+  --mqtt_client_id send_strava
